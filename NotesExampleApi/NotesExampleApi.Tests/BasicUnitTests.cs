c924e6585bc24e7914452591f20c315a38f0dc90
KirkM2338 <80219706+KirkM2338@users.noreply.github.com>
2021-03-07 21:10:04 -0500

    Added some postman validations.

--- NotesExampleApi/NotesExampleApi.Tests/BasicUnitTests.cs
+++ NotesExampleApi/NotesExampleApi.Tests/BasicUnitTests.cs
@@ -1,17 +1,48 @@
 using NUnit.Framework;
+using System.Collections.Generic;
+using NotesExampleApi.Models;
+
 
 namespace NotesExampleApi.Tests
 {
-    public class Tests
+    //These are just some basic unit tests. 
+
+    public class BasicUnitTests
     {
+        List<NoteModel> _modelsToTest;
+
         [SetUp]
         public void Setup()
         {
+            //For testing ill create a 1000 random models. 
+           _modelsToTest = DevelopTestingData.CreateNumberOfRandomNotes(1000);
         }
 
         [Test]
-        public void Test1()
+        public void CreateNewNote()
         {
+
+
+
+            Assert.Pass();
+        }
+
+
+        [Test]
+        public void EditNote()
+        {
+
+
+
+            Assert.Pass();
+        }
+
+        [Test]
+        public void DeleteNote()
+        {
+
+
+
             Assert.Pass();
         }
     }
